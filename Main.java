57309995adbf40fe569d42e5b45416b3b23a9bf7
Ondrej Krutsky <ondrej@krutsky.com>
2025-09-28 00:27:52 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package wms;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		InventoryService inventoryService = new InventoryService();
+		Item item1 = new Item("1", "Robot");
+		Item item2 = new Item("2", "Robot2");
+	}
+}
